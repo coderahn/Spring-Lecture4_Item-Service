6e4973142ab8c73f0890edc9edc63834f3c8cb34
ash2748 <max3535@naver.com>
2022-06-29 07:55:17 +0900

    스프링 MVC - 상품등록폼 및 상품등록처리(ModelAttribute 여러가지 방법의 활용)

--- src/main/java/hello/itemservice/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,6 +34,55 @@ public class BasicItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //ModelAttribute(네임)의 '네임'은 model.addAttribute 역할을 해준다. view에서 보여줄 attributeName이라고 볼 수 있다.
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //ModelAttribute("item")으로 인해 생략 가능
+
+        return "basic/item";
+    }
+
+    //ModelAttribute(네임)의 '네임'생략시 클래스네임 앞을 소문자로 바꿔서 model.addAttribute(네임,value)가 된다.
+    //ex)ModelAttribute Item item은 model.addAttribute("item", value)와 같다.
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //ModelAttribute("item")으로 인해 생략 가능
+
+        return "basic/item";
+    }
+
+    //ModelAttribute 생략 가능(String등 단순타입인 경우 생략하면 @RequestParam)
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
 
         return "basic/item";
     }
