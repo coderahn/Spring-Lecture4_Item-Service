9847896ab3a57cbee66e90b01b20ace9754aec12
ash2748 <max3535@naver.com>
2022-07-05 06:54:35 +0900

    스프링 MVC - 리다이렉트 때 모델쓰기 RedirectAttribute

--- src/main/java/hello/itemservice/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -88,10 +89,22 @@ public class BasicItemController {
     }
 
     //PRG 패턴 적용(저장로직 중복 방지)
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    //리다이렉트 후 model 처리할 수 있는 방버
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+
+        //{itemId}에 itemId가 들어간다. status는 쿼리파라미터 형태로 들어간다.
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     //상품수정폼
